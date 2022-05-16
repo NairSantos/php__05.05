8ed37545a60a47c19b4f3325942421faf260a59b
NairSantos <nair.sousa@etec.sp.gov.br>
2022-05-16 20:33:55 -0300

    php

---
+++ php 05.05/projeto4/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <tittle>Formulario</tittle>
+    </head>
+    <body>
+        <form action="resposta.php" method="GET">
+            <label for="">Número</label>
+            <input type="text" name="txtNumero">
+            <button>Enviar</button>
+</form>
+</body>
+</html>
